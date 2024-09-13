56d8d795ff8186d76a3cf849d4a5225b77d5b25a
Alexell <alexell@alexell.ru>
2024-09-13 15:57:33 +0500

    Реализована поддержка кастомных сигналов

--- lua/autorun/mss_ao_signals_rewrite.lua
+++ lua/autorun/mss_ao_signals_rewrite.lua
@@ -1,6 +1,17 @@
 
-hook.Add("InitPostEntity", "MSS.SignalsRewrite", function()
-    local ENT = scripted_ents.GetStored("gmod_track_signal").t
+hook.Add("Initialize", "MSS.SignalsRewrite", function()
+    local defaultClass = "gmod_track_signal"
+	local realClass = defaultClass
+	for k, v in pairs(scripted_ents.GetList()) do
+		if v.t.ClassName:find(defaultClass) and not v.t.ClassName:find("controller") then
+			if v.t.ClassName ~= defaultClass then
+				realClass = v.t.ClassName
+				break
+			end
+		end
+	end
+	MsgC(Color(0, 220, 0, 255), "MSS AO: Selected signal class: "..realClass.."\n")
+	local ENT = scripted_ents.GetStored(realClass).t
     local check_occ = ENT.CheckOccupation
     function ENT:CheckOccupation()
         check_occ(self)
